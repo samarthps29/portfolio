c41fe70568b908d706c80d48513cfbb67eef885f
samarthps29 <samarthps29@gmail.com>
2023-12-25 12:36:17 +0530

    Final Changes

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,25 +1,25 @@
 export const projectItems = [
 	{
 		title: "Developer Portfolio",
-		imgURL: "src/assets/1.png",
+		imgURL: "/assets/1.png",
 		stack: ["React", "Typescript", "Tailwind"],
 		link: "",
 	},
 	{
 		title: "Reflections",
-		imgURL: "src/assets/3.png",
+		imgURL: "/assets/3.png",
 		stack: ["React", "TypeScript", "ExpressJs"],
 		link: "https://reflections.cyclic.app/",
 	},
 	{
 		title: "Drinker",
-		imgURL: "src/assets/5.png",
+		imgURL: "/assets/5.png",
 		stack: ["React Native", "Typescript"],
 		link: "https://github.com/samarthps29/drinker",
 	},
 	{
 		title: "Speech-to-text",
-		imgURL: "src/assets/2.png",
+		imgURL: "/assets/2.png",
 		stack: ["Python", "MongoDB"],
 		link: "",
 	},
