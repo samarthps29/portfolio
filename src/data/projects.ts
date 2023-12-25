ebb3246eda3371c8cf8ef19594025945192c375b
samarthps29 <samarthps29@gmail.com>
2023-12-25 12:19:27 +0530

    Fixed bug where deployed website does not contain pictures

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,25 +1,25 @@
 export const projectItems = [
 	{
 		title: "Developer Portfolio",
-		imgURL: "public\\1.png",
+		imgURL: "src/assets/1.png",
 		stack: ["React", "Typescript", "Tailwind"],
 		link: "",
 	},
 	{
 		title: "Reflections",
-		imgURL: "public\\3.png",
+		imgURL: "src/assets/3.png",
 		stack: ["React", "TypeScript", "ExpressJs"],
 		link: "https://reflections.cyclic.app/",
 	},
 	{
 		title: "Drinker",
-		imgURL: "public\\5.png",
+		imgURL: "src/assets/5.png",
 		stack: ["React Native", "Typescript"],
 		link: "https://github.com/samarthps29/drinker",
 	},
 	{
 		title: "Speech-to-text",
-		imgURL: "public\\2.png",
+		imgURL: "src/assets/2.png",
 		stack: ["Python", "MongoDB"],
 		link: "",
 	},
